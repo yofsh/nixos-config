fac493bc22deaaba9e6df7c6877c78e41f8a7ae9
Oleksandr Yaroshenko <to@yof.sh>
2024-05-28 20:49:05 +0200

    steam and optimus added

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -191,6 +191,60 @@
     };
 
   programs.firefox.enable = true;
+  programs.steam.enable = true;
+  programs.steam.gamescopeSession.enable = true; 
+
+	hardware.nvidia.prime = {
+		offload = {
+			enable = true;
+			enableOffloadCmd = true;
+		};
+		# Make sure to use the correct Bus ID values for your system!
+		nvidiaBusId = "PCI:69:0:0";
+    amdgpuBusId = "PCI:01:0:0"; 
+	};
+
+ hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
 
   programs.firefox.policies =   {
         NewTabPage = false;
@@ -270,6 +324,27 @@ services.logind = {
   #   enable = true;
   # };
 
+
+
+#disable GPU
+# boot.extraModprobeConfig = ''
+#   blacklist nouveau
+#   options nouveau modeset=0
+# '';
+#   
+# services.udev.extraRules = ''
+#   # Remove NVIDIA USB xHCI Host Controller devices, if present
+#   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+#   # Remove NVIDIA USB Type-C UCSI devices, if present
+#   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+#   # Remove NVIDIA Audio devices, if present
+#   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+#   # Remove NVIDIA VGA/3D controller devices
+#   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+# '';
+# boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
+###
+
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
@@ -306,7 +381,22 @@ services.logind = {
   ];
 
   services.fstrim.enable = true;
-  services.fprintd = {
-    enable = true;
-  };
+ # networking.wg-quick.interfaces = {
+ #    wg0 = {
+ #      address = [ "192.168.99.2/24" ];
+ #      dns = [ "1.1.1.1" ];
+ #      privateKeyFile = "/root/wireguard-keys/privatekey";
+ #      
+ #      peers = [
+ #        {
+ #          publicKey = "{server public key}";
+ #          presharedKeyFile = "/root/wireguard-keys/preshared_from_peer0_key";
+ #          allowedIPs = [ "0.0.0.0/0" "::/0" ];
+ #          endpoint = "{server ip}:51820";
+ #          persistentKeepalive = 25;
+ #        }
+ #      ];
+ #    };
+ #  };
+
 }
