76befc480fb7eba63943d66e43a35d57d8e7c0f3
Oleksandr Yaroshenko <to@yof.sh>
2024-06-07 18:15:25 +0200

    updated config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -161,6 +161,8 @@
   networking.hostName = "laptop";
   networking.hosts = {
     "192.168.1.50" = [ "srv" ];
+    "192.168.99.1" = [ "wghost" ];
+    "116.203.205.147" = [ "vps" ];
   };
 
   time.timeZone = "Europe/Madrid";
@@ -202,15 +204,6 @@
   programs.steam.enable = true;
   programs.steam.gamescopeSession.enable = true; 
 
-  hardware.nvidia.prime = {
-    offload = {
-      enable = true;
-      enableOffloadCmd = true;
-    };
-# Make sure to use the correct Bus ID values for your system!
-    nvidiaBusId = "PCI:69:0:0";
-    amdgpuBusId = "PCI:01:0:0"; 
-  };
 
   hardware.opengl = {
     enable = true;
@@ -218,40 +211,6 @@
     driSupport32Bit = true;
   };
 
-# Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
-
-  hardware.nvidia = {
-
-# Modesetting is required.
-    modesetting.enable = true;
-
-# Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-# Enable this if you have graphical corruption issues or application crashes after waking
-# up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-# of just the bare essentials.
-    powerManagement.enable = false;
-
-# Fine-grained power management. Turns off GPU when not in use.
-# Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-# Use the NVidia open source kernel module (not to be confused with the
-# independent third-party "nouveau" open source driver).
-# Support is limited to the Turing and later architectures. Full list of 
-# supported GPUs is at: 
-# https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-# Only available from driver 515.43.04+
-# Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
-
-# Enable the Nvidia settings menu,
-# accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-# Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
 
 
   programs.firefox.policies =   {
@@ -338,23 +297,41 @@
 
 
 #disable GPU
-# boot.extraModprobeConfig = ''
-#   blacklist nouveau
-#   options nouveau modeset=0
-# '';
-#   
-# services.udev.extraRules = ''
-#   # Remove NVIDIA USB xHCI Host Controller devices, if present
-#   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
-#   # Remove NVIDIA USB Type-C UCSI devices, if present
-#   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
-#   # Remove NVIDIA Audio devices, if present
-#   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
-#   # Remove NVIDIA VGA/3D controller devices
-#   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
-# '';
-# boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
-###
+boot.extraModprobeConfig = ''
+  blacklist nouveau
+  options nouveau modeset=0
+'';
+  
+services.udev.extraRules = ''
+  # Remove NVIDIA USB xHCI Host Controller devices, if present
+  ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+  # Remove NVIDIA USB Type-C UCSI devices, if present
+  ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+  # Remove NVIDIA Audio devices, if present
+  ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+  # Remove NVIDIA VGA/3D controller devices
+  ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+'';
+boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
+
+
+  # hardware.nvidia.prime = {
+  #   offload = {
+  #     enable = true;
+  #     enableOffloadCmd = true;
+  #   };
+  #   nvidiaBusId = "PCI:69:0:0";
+  #   amdgpuBusId = "PCI:01:0:0"; 
+  # };
+  # services.xserver.videoDrivers = ["nvidia"];
+  # hardware.nvidia = {
+  #   modesetting.enable = true;
+  #   powerManagement.enable = false;
+  #   powerManagement.finegrained = true;
+  #   open = false;
+  #   nvidiaSettings = true;
+  #   package = config.boot.kernelPackages.nvidiaPackages.stable;
+  # };
 
   hardware.bluetooth = {
     enable = true;
