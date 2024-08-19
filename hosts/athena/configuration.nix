8091c1358e47bba3523799dd7c4dcdef42e9d597
Oleksandr Yaroshenko <to@yof.sh>
2024-08-19 16:10:47 +0200

    reformated athena config

--- hosts/athena/configuration.nix
+++ hosts/athena/configuration.nix
@@ -1,52 +1,57 @@
-{
-  config,
-    lib,
-    pkgs,
-    ...
-}: {
-  imports = [
-    ./hardware-configuration.nix
-    ./../../modules/base.nix
-  ];
+{ config, lib, pkgs, ... }: {
+  imports = [ ./hardware-configuration.nix ./../../modules/base.nix ];
 
-
-  nixpkgs.config = {
-    allowUnfree = true;
-  };
+  nixpkgs.config = { allowUnfree = true; };
 
   environment.systemPackages = with pkgs; [
-      (google-chrome.override {
-       commandLineArgs = [
-       "--enable-features=UseOzonePlatform"
-       "--ozone-platform=wayland"
-       ];
-       })
-      foot
-      mpv
-      imv
-      krita
-      yubikey-personalization
-      gnupg
-      orca-slicer
-# Coding stuff
+    (google-chrome.override {
+      commandLineArgs =
+        [ "--enable-features=UseOzonePlatform" "--ozone-platform=wayland" ];
+    })
 
-#TUI utils
+    #lsps move to a separate modules
+    stylua
+    nixfmt
+    shfmt
+    yq
+    lua-language-server
+    yaml-language-server
+    bash-language-server
+    typescript-language-server
+    # nodePackages.vscode-json-languageserver
+    # nodePackages.eslint
+    bash-language-server
+    stylua
+    nixd
+
+    vscode-langservers-extracted
+
+    obsidian
+    foot
+    mpv
+    imv
+    krita
+    yubikey-personalization
+    gnupg
+    orca-slicer
+    # Coding stuff
+
+    #TUI utils
     bluetuith
     powertop
 
-# CLI utils
+    # CLI utils
     proxmark3
     rbw
     bluez
     bluez-tools
     udiskie
 
+    # GUI utils
 
-# GUI utils
+    # Wayland stuff
 
-# Wayland stuff
-
-# WMs and stuff
+    # WMs and stuff
     pyprland
     hyprpicker
     hyprcursor
@@ -68,15 +73,15 @@
     wtype
     wev
 
-# Sound
+    # Sound
     pipewire
     pulseaudio
     ncpamixer
     usbutils
 
-# GPU stuff
+    # GPU stuff
 
-# Screenshotting
+    # Screenshotting
     grim
     slurp
     satty
@@ -84,23 +89,23 @@
     aichat
     plymouth
 
-# Other
+    # Other
     home-manager
     materia-theme
     papirus-icon-theme
 
     godot_4
-    ];
+  ];
 
   fonts.packages = with pkgs; [
     jetbrains-mono
-      noto-fonts
-      noto-fonts-emoji
-      twemoji-color-font
-      font-awesome
-      powerline-fonts
-      powerline-symbols
-      (nerdfonts.override {fonts = ["NerdFontsSymbolsOnly"];})
+    noto-fonts
+    noto-fonts-emoji
+    twemoji-color-font
+    font-awesome
+    powerline-fonts
+    powerline-symbols
+    (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
   ];
 
   services.udisks2.enable = true;
@@ -112,9 +117,7 @@
     enableSSHSupport = true;
   };
 
-  security.polkit = {
-    enable = true;
-  };
+  security.polkit = { enable = true; };
 
   environment.shellInit = ''
     gpg-connect-agent /bye
@@ -127,12 +130,11 @@
   services.interception-tools.enable = true;
 
   services.interception-tools.udevmonConfig = ''
-- JOB: "intercept -g $DEVNODE | uinput -d $DEVNODE"
-DEVICE:
-  EVENTS:
-    EV_KEY: [KEY_LAUNCH2]
+    - JOB: "intercept -g $DEVNODE | uinput -d $DEVNODE"
+    DEVICE:
+      EVENTS:
+        EV_KEY: [KEY_LAUNCH2]
   '';
-
 
   networking.hostName = "laptop";
   networking.hosts = {
@@ -144,25 +146,20 @@ DEVICE:
   time.timeZone = "Europe/Madrid";
   i18n.defaultLocale = "en_US.UTF-8";
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   system.stateVersion = "23.05";
 
   virtualisation.docker.enable = true;
-  programs.virt-manager = {
-    enable = true;
-  };
+  programs.virt-manager = { enable = true; };
   services.asusd.enable = true;
   services.upower.enable = true;
   programs.light.enable = true;
-
-
 
   programs.noisetorch.enable = true;
 
   programs.firefox.enable = true;
   programs.steam.enable = true;
-  programs.steam.gamescopeSession.enable = true; 
-
+  programs.steam.gamescopeSession.enable = true;
 
   hardware.opengl = {
     enable = true;
@@ -170,7 +167,7 @@ DEVICE:
     # driSupport32Bit = true;
   };
 
-  programs.firefox.policies =   {
+  programs.firefox.policies = {
     NewTabPage = false;
     CaptivePortal = true;
     DisableFirefoxStudies = true;
@@ -192,11 +189,14 @@ DEVICE:
       SkipOnboarding = true;
     };
     Preferences = {
-      "ui.key.menuAccessKeyFocuses" = { Status = "locked"; Value = false; };
+      "ui.key.menuAccessKeyFocuses" = {
+        Status = "locked";
+        Value = false;
+      };
     };
   };
 
-#Hyprland
+  #Hyprland
   programs.hyprland.enable = true;
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
@@ -209,44 +209,40 @@ DEVICE:
   #   lidSwitch = "ignore";
   # };
 
-
-#boot
+  #boot
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.loader.timeout = 1;
-# boot.initrd.verbose = false;
-# boot.consoleLogLevel = 0;
-# boot.kernelParams = ["quiet" "udev.log_level=0" "amdgpu"];
-  boot.kernelParams = ["amdgpu" "amdgpu.dcdebugmask=0x10"];
-# boot.plymouth = {
-#   enable = true;
-# };
+  # boot.initrd.verbose = false;
+  # boot.consoleLogLevel = 0;
+  # boot.kernelParams = ["quiet" "udev.log_level=0" "amdgpu"];
+  boot.kernelParams = [ "amdgpu" "amdgpu.dcdebugmask=0x10" ];
+  # boot.plymouth = {
+  #   enable = true;
+  # };
 
+  #disable GPU
+  boot.extraModprobeConfig = ''
+    blacklist nouveau
+    options nouveau modeset=0
+  '';
 
+  services.udev.extraRules = ''
+    # Remove NVIDIA USB xHCI Host Controller devices, if present
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+    # Remove NVIDIA USB Type-C UCSI devices, if present
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+    # Remove NVIDIA Audio devices, if present
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+    # Remove NVIDIA VGA/3D controller devices
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
 
-#disable GPU
-boot.extraModprobeConfig = ''
-  blacklist nouveau
-  options nouveau modeset=0
-'';
-  
-services.udev.extraRules = ''
-  # Remove NVIDIA USB xHCI Host Controller devices, if present
-  ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
-  # Remove NVIDIA USB Type-C UCSI devices, if present
-  ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
-  # Remove NVIDIA Audio devices, if present
-  ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
-  # Remove NVIDIA VGA/3D controller devices
-  ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+    KERNEL=="ttyACM0", TAG+="uaccess", SYMLINK+="ttyACM0_fobos", MODE="0660", OWNER="fobos"
+  '';
 
-  KERNEL=="ttyACM0", TAG+="uaccess", SYMLINK+="ttyACM0_fobos", MODE="0660", OWNER="fobos"
-'';
-
-
-boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
-
+  boot.blacklistedKernelModules =
+    [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
 
   # hardware.nvidia.prime = {
   #   offload = {
@@ -285,19 +281,20 @@ boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modese
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-# If you want to use JACK applications, uncomment this
+    # If you want to use JACK applications, uncomment this
     jack.enable = true;
   };
 
   services.pipewire.wireplumber.configPackages = [
-    (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
-     bluez_monitor.properties = {
-     ["bluez5.enable-sbc-xq"] = true,
-     ["bluez5.enable-msbc"] = true,
-     ["bluez5.enable-hw-volume"] = true,
-     ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-     }
-     '')
+    (pkgs.writeTextDir
+      "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
+        bluez_monitor.properties = {
+        ["bluez5.enable-sbc-xq"] = true,
+        ["bluez5.enable-msbc"] = true,
+        ["bluez5.enable-hw-volume"] = true,
+        ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+        }
+      '')
   ];
 
   services.fstrim.enable = true;
@@ -307,28 +304,24 @@ boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modese
       address = [ "192.168.99.20/24" ];
       dns = [ "1.1.1.1" ];
       privateKeyFile = "/home/fobos/wg";
-      peers = [
-      {
+      peers = [{
         publicKey = "NcAhvpiJJQEttjJdP8aT4DJocX6jZObv4cBJiakZt3w=";
         allowedIPs = [ "0.0.0.0/0" "::/0" ];
         endpoint = "116.203.205.147:51820";
         persistentKeepalive = 25;
-      }
-      ];
+      }];
     };
     medusaWG = {
       autostart = false;
       address = [ "10.100.0.2" ];
       dns = [ "1.1.1.1" ];
       privateKeyFile = "/home/fobos/medusawg";
-      peers = [
-      {
+      peers = [{
         publicKey = "CTeiSpj47ioFxSFTe9nVk0dZXLmoLpGxRWbwZ3wWXgk=";
         allowedIPs = [ "0.0.0.0/0" "::/0" ];
         endpoint = "65.21.153.166:51820";
         persistentKeepalive = 25;
-      }
-      ];
+      }];
     };
   };
   services.ollama = {
